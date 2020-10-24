0a0962dedd108563b4c46da5e301cc4220813d5d
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-10-24 04:07:21 +0300

    setup packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="decorator-factory",
+    version="0.0.1", # don't change version for now, early development
+    author="decorator-factory",
+    author_email="appendix.y.z@gmail.com",
+    description="Terse and extensible markup language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/decorator-factory/fnl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "lark",
+    ],
+    python_requires='>=3.7',
+    package_data={
+        "": ["*.md", "*.lark", "*.html"]
+    }
+)
